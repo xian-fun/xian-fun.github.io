b8aac36b662d6bf1596a8c3b91f9c77fbcf8a3ef
zhao-guo <zhao.guo@rea-group.com>
2015-02-12 14:28:31 +0800

    basic frame

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ entertainment-map
 =================
 
 For hackday #18!
+
+1. Guardfile to watch changes
+2. config.rb used for compass
+
+To use it, simply clone this repo and :
+
+```sh
+$ bundle install
+```
+
+After the required `gem`s are installed, you can then run
+
+```sh
+$ guard start
---
+++ javascript/entertainment_data.js
@@ -0,0 +1,62 @@
+var entertainmentData = [
+  {
+    type: 1,
+    rate: 99,
+    name: 'Green Town',
+    telephone: '12345678',
+    location:
+    {
+      address: 'No 2, West Street',
+      latitude: 34.1111,
+      longtitude: 105.2222
+    },
+    image:
+    {
+      file_name: 'green_town.jpg',
+      link: 'http://green-town.com'
+    },
+    comments:
+      [
+        {
+          author: 'JP',
+          avartar: 'jp.jpg',
+          content: 'Nice!'
+        },
+        {
+          author: 'Yongmin',
+          avartar: 'yongmin.jpg',
+          content: 'Nice +1!'
+        }
+      ]
+  },
+  {
+    type: 1,
+    rate: 99,
+    name: 'Green Town',
+    telephone: '12345678',
+    location:
+    {
+      address: 'No 2, West Street',
+      latitude: 34.1111,
+      longtitude: 105.2222
+    },
+    image:
+    {
+      file_name: 'green_town.jpg',
+      link: 'http://green-town.com'
+    },
+    comments:
+      [
+        {
+          author: 'JP',
+          avartar: 'jp.jpg',
+          content: 'Nice!'
+        },
+        {
+          author: 'Yongmin',
+          avartar: 'yongmin.jpg',
+          content: 'Nice +1!'
+        }
+      ]
+  }
+]
